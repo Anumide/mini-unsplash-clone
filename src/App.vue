c94bdcbadc0ecae9157955ed3c6c0ea25910b8d4
anumide <alabasamuelayomide3@gmail.com>
2024-10-07 14:30:04 +0100

    feat: header styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+import SearchBar from '@/components/SearchBar.vue'
+
+const searhValue = ref()
+const loading = ref(false)
+const searchText = ref("")
+function search(value: string) {
+  console.log(value)
+}
 </script>
 
 <template>
-  <div>
-    Hello World!
+  <div class="">
+    <div class="header">
+      <div class="container">
+        <SearchBar
+          v-if="!loading"
+          v-model="searhValue" 
+          @change.prevent="search(searhValue)" 
+        />
+        <div v-else class="search--states">
+          <p>Search Results for <span>"Fun"</span></p>
+        </div>
+      </div>
+    </div>
   </div>
 
   <RouterView />
---
+++ src/components/searchBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="search--input">
+    <SearchIcon />
+    <input 
+      v-model="model" 
+      class="search-input" 
+      type="search"
+      placeholder="Search for photo"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineModel } from 'vue'
+import SearchIcon from '@/components/icons/SearchIcon.vue'
+
+const model = defineModel()
+</script>
